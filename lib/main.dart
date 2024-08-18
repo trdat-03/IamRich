a7a63758a328f0db6d80b04022a2bf1349eb9633
trdat-03 <trdat1812@gmail.com>
2024-08-18 11:08:56 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonName = "Click dzo";
+  int currentIndex = 0;
+  double maxWidth = double.infinity;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("I Am Rich"),
+        ),
+        body: Container(
+            width: maxWidth,
+            height: maxWidth,
+            color: Colors.pink,
+            child: const Image(
+              image: AssetImage('assets/dinamond.png'),
+            )),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
